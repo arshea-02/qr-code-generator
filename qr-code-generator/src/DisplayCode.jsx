16cdc1a083108572e43dc54359b24e2c54ed1cf4
internArshea <arshea.intern@devsinc.com>
2024-07-23 16:24:24 +0500

    Download Button

--- qr-code-generator/src/DisplayCode.jsx
+++ qr-code-generator/src/DisplayCode.jsx
@@ -1,9 +1,11 @@
 const DisplayCode = ({qrCodeURL}) =>{
-    
+   
     return(
         <>
             {qrCodeURL ? 
-                (<><img src={qrCodeURL} alt='QRCode'/><p>Scan QR Code</p></>) :
+                (<><img src={qrCodeURL} alt='QRCode'/><p>Scan QR Code</p>
+                <a href={qrCodeURL} download='QRcode'>
+                    <button type='button'>Save QR</button></a></>) :
                 (<p>Enter a URL to generate a QR Code</p>)
             }
         </>   
